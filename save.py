c8e9a6ad143b2495330b88788ec651d304080188
Sky <swo5157@psu.edu>
2017-09-20 12:49:12 +0000

    Added save ability to tables

--- save.py
+++ save.py
@@ -1,13 +1,34 @@
 import csv
+csv.register_dialect('hockdia', delimiter=':')
 import os
-#def saveTable():
-
-#def loadTable():
+from scraper import *
+def saveTable(tableData):
+    table = tableData.stats
+    name = tableData.name + '.csv'
+    name = name.replace(" ", "_")
+    with open("Saves/" + name, "w") as f:
+        writer = csv.writer(f,'hockdia')
+        writer.writerows(table)
+    
+def loadTable(name):
+    name = name + '.csv'
+    name = name.replace(' ', '_')
+    with open("Saves/" + name, 'r') as r:
+        reader = csv.reader(r,'hockdia')
+        table = list(reader)
+    return table
+    
 
 def checkIfDirExists():
     if not os.path.exists("Saves"):
         os.makedirs("Saves")
-    else:
-        print("DIR EXISTS")
-        
-checkIfDirExists()
+
+if __name__ == '__main__':
+    url = findPlayer("Ovechkin")
+    page = connectToPage(url)
+    print(page[0])
+    exit()
+    #TableData object is returned by getTables
+    tables = getTables(page)
+    loadTable('NHL_Standard')
+    
