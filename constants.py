c476b3ae28015b382395930bf0bd5ca68029e125
Sky <swo5157@psu.edu>
2017-10-09 12:43:23 +0000

    Added constants for other sports

---
+++ constants.py
@@ -0,0 +1,19 @@
+MLB_BASE = "https://www.baseball-reference.com/"
+MLB_SEARCH = "https://www.baseball-reference.com/search/search.fcgi?hint=&search="
+
+NBA_BASE = "https://www.basketball-reference.com/"
+NBA_SEARCH = "https://www.basketball-reference.com/search/search.fcgi?hint=&search="
+
+NFL_BASE = "https://www.pro-football-reference.com/"
+NFL_SEARCH = "https://www.pro-football-reference.com/search/search.fcgi?hint=&search="
+
+NHL_BASE = "https://www.hockey-reference.com/"
+NHL_SEARCH = "https://www.hockey-reference.com/search/search.fcgi?hint=&search="
+
+#College football
+CFB_BASE = "https://www.sports-reference.com/cfb/"
+CFB_SEARCH = "https://www.sports-reference.com/cfb/search/search.fcgi?hint=&search="
+
+#College basketball
+CBB_BASE = "https://www.sports-reference.com/cbb/"
+CBB_SEARCH = "https://www.sports-reference.com/cbb/search/search.fcgi?hint=&search="
